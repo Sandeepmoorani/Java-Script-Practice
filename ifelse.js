e328f6fd31303ef3e13aaaaca6456138a27fd417
Sandeepmoorani <21sw015@students.muet.edu.pk>
2024-03-12 18:24:20 +0500

    ifelse code added

---
+++ ifelse.js
@@ -0,0 +1,21 @@
+// if syntx
+//if(condition){
+    // code
+//}
+
+var a = 10;
+if(a>1){
+    console.log("a is greater than 7");
+}
+
+
+
+var a =200;
+
+if(a%2==0){
+    console.log("a is even");
+}
+else{
+    console.log("a is odd");
+}
+
