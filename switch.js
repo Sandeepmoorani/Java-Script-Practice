8f36777f2506245cbfad02e1e7fe1c48cc40c4c6
Sandeepmoorani <21sw015@students.muet.edu.pk>
2024-03-12 18:29:50 +0500

    switch case added

---
+++ switch.js
@@ -0,0 +1,16 @@
+var a;
+switch(a=9){
+    case 1:
+        console.log("a is 1");
+        break;
+    case 2:
+        console.log("a is 2");
+        break;
+    case 3:
+        console.log("a is 3");
+        break;
+    default:
+        console.log("a is not 1, 2 or 3");
+        break;
+
+}
