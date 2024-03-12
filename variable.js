c2a257fc141c090f958b97920d749806bb869d85
Sandeepmoorani <21sw015@students.muet.edu.pk>
2024-03-12 18:09:52 +0500

    variable in js

---
+++ variable.js
@@ -0,0 +1,17 @@
+var a= 10;
+console.log(a);
+
+
+var a= 10;
+var b = 20;
+var c = 30;
+var d = a+b+c;
+console.log(d);
+
+// local variable
+
+function var1(){
+    var x= 5;
+    console.log(x);
+}
+var1();
