f1f67b29678e03ce0bf733b1dc752bb3aa8f0418
Sandeepmoorani <21sw015@students.muet.edu.pk>
2024-03-13 16:27:34 +0500

    added loops code

---
+++ loops.js
@@ -0,0 +1,25 @@
+// for loop
+
+for(i=1; i<=10; i++){
+    // console.log(i);
+    document.write(i + "<br/>");
+}
+
+
+
+//while loop
+
+var a = 1;
+while(a<=10){
+    document.write(a + "<br/>");
+    a++;
+}
+
+
+// do while loop
+
+var b = 1;
+do{
+    document.write(b + "<br/>");
+    b++;
+}while(b<=15);
