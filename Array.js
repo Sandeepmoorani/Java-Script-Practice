6539b91c87dcf761d2be914be2146185197a5fc8
Sandeepmoorani <21sw015@students.muet.edu.pk>
2024-03-14 13:21:54 +0500

    another method og creating array and also with diff data types

--- Array.js
+++ Array.js
@@ -8,4 +8,23 @@ for(i= 0; i<=4; i++){
 
     sum = sum + arr[i];
 }
-document.write("Sum of array is: " + sum);
+document.write("Sum of array is: " + sum + "<br>");
+
+
+
+// another way to write array
+
+var arr1 = new Array();
+arr1[0]= "Sandeep";
+arr1[1]= "Raj";
+arr1[2]= "Rohit";
+
+for(i=0; i<3; i++){
+    document.write(arr1[i] + "<br>");
+}
+
+// Array with different data types
+var arr2 = [true, "Sandeep", null, "Raj", 3, "Rohit"];
+for(i=0; i<6; i++){
+    document.write(arr2[i] + "<br>");
+}
