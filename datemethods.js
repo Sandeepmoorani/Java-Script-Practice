e59659cf1dd4d9574a2159163f342a0e6bc055f5
Sandeepmoorani <21sw015@students.muet.edu.pk>
2024-03-17 22:57:40 +0500

    added date methods

---
+++ datemethods.js
@@ -0,0 +1,20 @@
+var dat = new Date();
+document.write(dat+"<br>");
+
+document.write(dat.toDateString()+"<br>");
+
+var now = new Date('March 11 2003');
+document.write(now.getDay()+"<br>");
+document.write(now.getMonth()+"<br>");
+document.write(now.getFullYear()+"<br>");
+
+
+
+
+
+
+
+
+
+
+
