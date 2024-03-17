4541985723fec84af24cb0136cc5a1748b961094
Sandeepmoorani <21sw015@students.muet.edu.pk>
2024-03-17 22:45:35 +0500

    added mathmethods which are used in js

---
+++ mathmethods.js
@@ -0,0 +1,38 @@
+var a = Math.ceil(1.2);
+document.write(a + "<br>" );
+
+var a = Math.floor(2.3);
+document.write(a + "<br>");
+
+var a = Math.round(3.5);
+document.write(a +"<br>")
+
+var a = Math.max(2,4,6,8,10);
+document.write(a+"<br>");
+
+var a =Math.min(1,2,3,4,5,6,0.1,0.3);
+document.write(a+"<br>");
+
+var a = Math.random();
+document.write(a+"<br>");
+
+var a = Math.sqrt(16);
+document.write(a+"<br>");
+
+var a = Math.pow(5,2);
+document.write(a+"<br>");
+
+var a = Math.abs(-5);
+document.write(a+"<br>");
+
+
+
+
+
+
+
+
+
+
+
+
