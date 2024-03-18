090149f209e0da1cf296eadfec03c6e517d85d8f
Sandeepmoorani <21sw015@students.muet.edu.pk>
2024-03-18 20:39:59 +0500

    how to target the elements in dom

---
+++ DOM.js
@@ -0,0 +1,22 @@
+//DOM in JS
+
+//DOM is a programming interface for HTML and XML documents. 
+// It represents the page so that programs can change the document structure, style, and content. 
+// The DOM represents the document as nodes and objects. That way, programming languages can connect to the page.
+
+
+// how to target elements in the DOM
+
+// 1. getElementById
+// 2. getElementsByClassName
+// 3. getElementsByTagName
+// 4. querySelector
+
+
+var element;
+// element = document;
+element = document.head;
+element = document.body;
+element = document.title;
+element = document.links;
+console.log(element);
